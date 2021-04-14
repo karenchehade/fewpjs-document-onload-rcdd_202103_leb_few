19463c9ef18a5c3ed908eee7f81dbd75573d4e0b
karen chehade <karen_chehade@hotmail.com>
2021-04-14 10:14:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
   console.log("The DOM has loaded");
 });
+console.log(
+  "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+);
