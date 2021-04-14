112f6fd368c2365af9ab80fc1a8e892e3e673d39
karen chehade <karen_chehade@hotmail.com>
2021-04-14 10:31:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-  console.log("The DOM has loaded");
+  document,getElementById("text").textContent = "This is really cool!";
 });
 
 console.log(
